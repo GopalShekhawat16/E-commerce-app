d032cf125113a5304cfcc1026e2c8af553078f3a
GopalShekhawat16 <162530951+GopalShekhawat16@users.noreply.github.com>
2025-07-22 17:34:09 +0530

    Update AboutPage.jsx

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -8,19 +8,11 @@ const AboutPage = () => {
         <h1 className="text-center">About Us</h1>
         <hr />
         <p className="lead text-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-          facere doloremque veritatis odit similique sequi. Odit amet fuga nam
-          quam quasi facilis sed doloremque saepe sint perspiciatis explicabo
-          totam vero quas provident ipsam, veritatis nostrum velit quos
-          recusandae est mollitia esse fugit dolore laudantium. Ex vel explicabo
-          earum unde eligendi autem praesentium, doloremque distinctio nesciunt
-          porro tempore quis eaque labore voluptatibus ea necessitatibus
-          exercitationem tempora molestias. Ad consequuntur veniam sequi ullam
-          tempore vel tenetur soluta dolore sunt maxime aliquam corporis est,
-          quo saepe dolorem optio minus sint nemo totam dolorum! Reprehenderit
-          delectus expedita a alias nam recusandae illo debitis repellat libero,
-          quasi explicabo molestiae saepe, dolorem tempore itaque eveniet quam
-          dignissimos blanditiis excepturi harum numquam vel nihil? Ipsum
+          Welcome to Shopprs, your trusted destination for the latest in fashion and electronics.
+We deliver top-quality clothing and gadgets right to your doorstep, with a focus on style, speed, and service.
+At Shopprs, we believe shopping should be smart, simple, and satisfying.
+From trendy wearables to cutting-edge devices, we’ve got it all under one roof.
+Join thousands of happy customers who choose Shopprs for a seamless online shopping experience.
         </p>
 
         <h2 className="text-center py-4">Our Products</h2>
